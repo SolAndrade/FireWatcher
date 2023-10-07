719893a482b221ae588df152a743ddd9b3d59842
José Manuel <115642154+josesanc02@users.noreply.github.com>
2023-10-07 14:36:04 +0200

    Header and navbar components

--- FireWatcherApp/src/app/app.module.ts
+++ FireWatcherApp/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeMapComponent } from './Home/home-map/home-map.component';
 import { HomeInfoComponent } from './Home/home-info/home-info.component';
 import { HomeReportComponent } from './Home/home-report/home-report.component';
 import { PopUpStartComponent } from './pop-up-start/pop-up-start.component';
-import { PopUpReportComponent } from './pop-up-report/pop-up-report.component';
+import { PopUpReportComponent } from './Home/home-report/pop-up-report/pop-up-report.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { PopUpReportComponent } from './pop-up-report/pop-up-report.component';
     HomeInfoComponent,
     HomeReportComponent,
     PopUpStartComponent,
-    PopUpReportComponent
+    PopUpReportComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
