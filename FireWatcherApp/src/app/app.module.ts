8711042c250b5926efe55fee0157f474e3673278
SolAndrade <solnicandrade@gmail.com>
2023-10-07 12:13:17 +0200

    [UPDATE] Folder organization

--- FireWatcherApp/src/app/app.module.ts
+++ FireWatcherApp/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeMapComponent } from './home-map/home-map.component';
-import { HomeInfoComponent } from './home-info/home-info.component';
-import { HomeReportComponent } from './home-report/home-report.component';
+import { HomeMapComponent } from './Home/home-map/home-map.component';
+import { HomeInfoComponent } from './Home/home-info/home-info.component';
+import { HomeReportComponent } from './Home/home-report/home-report.component';
 import { PopUpStartComponent } from './pop-up-start/pop-up-start.component';
 import { PopUpReportComponent } from './pop-up-report/pop-up-report.component';
 
